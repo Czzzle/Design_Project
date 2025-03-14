8246370ea94de1e2368e619e75d92fc125694229
Czzzle <131529327+Czzzle@users.noreply.github.com>
2025-03-14 09:39:58 -0400

    Change buffer size to 100*512  and send 's' to get 512 bytes. Now we can successfully transmit 60000 samples but DAC only gives 10 seconds output instead of 30s.

--- usb_serial_try.ino
+++ usb_serial_try.ino
@@ -23,8 +23,9 @@ volatile bool toggle = false; // Toggle flag for square wave
 volatile bool receiveSample = false; // initally, no samples will be considered.
 volatile bool timerStart = false; //initially, no timer is working
 
-#define MAX_BUFFER_SIZE 5120
+#define MAX_BUFFER_SIZE 51200
 #define HLAF_MAX_BUFFER_SIZE MAX_BUFFER_SIZE/2
+#define PACKET_SIZE 512
 
 uint8_t byte_buffer[MAX_BUFFER_SIZE]; //byte buffer (6*512 +2)
 volatile int nextRead = 0;
@@ -93,88 +94,135 @@ void loop() {
       nextWrite = 0;
       buffer_size = 0;
 
-      uint8_t ACK = 'S';
-      usb_serial_write(&ACK, 1);
+      // uint8_t ACK = 'S';
+      // usb_serial_write(&ACK, 1);
 
       receiveSample = true;
 
 
     }
   }else if (receiveSample == true && timerStart == false){
+    //timer is not working so buffer_size is only incrementing
+
     // fill the first half buffer
-    while(buffer_size < (HLAF_MAX_BUFFER_SIZE) && end_of_file == false){ 
+    while(buffer_size <= (HLAF_MAX_BUFFER_SIZE - PACKET_SIZE) && end_of_file == false){ 
+      //========= step 1: step 'S' to PC =======
+      uint8_t ACK = 'S';
+      usb_serial_write(&ACK, 1);
+
+      //========= step 2: wait for coming bytes =======
+      // Note: Use serial_avaible to continueouly check the coming data
+      while(usb_serial_available() == 0){
+        delay(0.2);
+        continue;
+        //do nothing just wait for coming bytes
+      }
+
+      //========= step 3: recive data and check for EOF by number of bytes =========
       coming_size = usb_serial_available();
 
-      if (coming_size == 512){ // normal sample data
+      if (coming_size == 512){ // normal sample data(as we asked for 512 packet_size)
         // digitalWrite(LED2, LOW);
-        int count = usb_serial_read(&byte_buffer[nextWrite], coming_size);
-        nextWrite += count;
+        int count = usb_serial_read(&byte_buffer[nextWrite], coming_size); //count should be the same as coming_size, just in case
+        // nextWrite += count;
+        nextWrite = (nextWrite + count) % MAX_BUFFER_SIZE;
         buffer_size += count;
 
-      }else if(coming_size > 0){ // tail data
+      }else if(coming_size > 0){ // tail data, and stop reciving data
         int count = usb_serial_read(&byte_buffer[nextWrite], coming_size);
-        nextWrite += count;
+        // nextWrite += count;
+        nextWrite = (nextWrite + count) % MAX_BUFFER_SIZE;
         buffer_size += count;
         digitalWrite(LED2, LOW); //recive end_of_file signal
         end_of_file = true;
-        break;
+        receiveSample = false;
+        break; // Leave while loop
       }
-    }
+    } //end while 
 
-    //if we recive smaller than 512 bytes, we reach end_of_file and we need to stop sample reciving
-    if(buffer_size < HLAF_MAX_BUFFER_SIZE){
-      receiveSample = false;
-    }
-    
-
+    //======== step 4: start timer to work ========
     timerStart = true;
     // Configure the timer for the desired period
     timer.begin(timerCallback, periodMicros);
 
-    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_RED, HIGH); // indication for timer start
+
   }
+
   else if (receiveSample == true && timerStart == true){
+    //************** reciving sample and timer working at the same time *****************
+
     //we need to fill the first half buffer
     if(nextRead >=HLAF_MAX_BUFFER_SIZE && nextWrite == 0){ 
-      uint8_t ACK = 'S';
-      usb_serial_write(&ACK, 1);
 
       while(nextWrite != HLAF_MAX_BUFFER_SIZE){ 
+        // ======= step 1: send 'S' to ask for samples from PC ===========
+        uint8_t ACK = 'S';
+        usb_serial_write(&ACK, 1);
+
+        //========= step 2: wait for coming bytes =======
+        // Note: Use serial_avaible to continueouly check the coming data
+        while(usb_serial_available() == 0){
+          delay(0.2);
+          //do nothing just wait for coming bytes
+        }
+        
+        //========= step 3: recive data and check for EOF by number of bytes =========
         coming_size = usb_serial_available();
 
         if (coming_size == 512){ // normal sample data
-
           int count = usb_serial_read(&byte_buffer[nextWrite], coming_size);
-          nextWrite = (nextWrite + count) % MAX_BUFFER_SIZE;;
+          nextWrite = (nextWrite + count) % MAX_BUFFER_SIZE; 
           buffer_size += count;
 
-        }else if(coming_size > 0){
+        }
+        else if(coming_size > 0){
           int count = usb_serial_read(&byte_buffer[nextWrite], coming_size);
-          nextWrite = (nextWrite + count) % MAX_BUFFER_SIZE;;
+          nextWrite = (nextWrite + count) % MAX_BUFFER_SIZE;
           buffer_size += count;
           digitalWrite(LED2, LOW); //recive end_of_file signal
           end_of_file = true;
           receiveSample = false;
           break;
         }
-      }
+
+        // if (usb_serial_available() == 0){
+        //   digitalWrite(LED_YELLOW, HIGH);
+        // }else{
+        //   digitalWrite(LED_YELLOW, LOW);
+        // }
+      } //end while
+
     }else if(nextRead < HLAF_MAX_BUFFER_SIZE && nextWrite == HLAF_MAX_BUFFER_SIZE){
       uint8_t ACK = 'S';
       usb_serial_write(&ACK, 1);
 
       while(nextWrite != 0 && end_of_file == false){ 
+        // ======= step 1: send 'S' to ask for samples from PC ===========
+        uint8_t ACK = 'S';
+        usb_serial_write(&ACK, 1);
+
+        //========= step 2: wait for coming bytes =======
+        // Note: Use serial_avaible to continueouly check the coming data
+        while(usb_serial_available() == 0){
+          delay(0.2);
+          continue;
+          //do nothing just wait for coming bytes
+        }
+        
+        //========= step 3: recive data and check for EOF by number of bytes =========
         coming_size = usb_serial_available();
 
         if (coming_size == 512){ // normal sample data
           digitalWrite(LED2, LOW);
           int count = usb_serial_read(&byte_buffer[nextWrite], coming_size);
-          nextWrite += count;
+          // nextWrite += count;
           nextWrite = nextWrite % MAX_BUFFER_SIZE;
           buffer_size += count;
 
         }else if(coming_size > 0 ){ //reach end_of_file
           int count = usb_serial_read(&byte_buffer[nextWrite], coming_size);
-          nextWrite += count;
+          // nextWrite += count;
           nextWrite = nextWrite % MAX_BUFFER_SIZE;
           buffer_size += count;
           digitalWrite(LED2, LOW); //recive end_of_file signal
@@ -182,17 +230,26 @@ void loop() {
           receiveSample = false;
           break;
         }
-      }
+
+        // if (usb_serial_available() == 0){
+        //   digitalWrite(LED_YELLOW, HIGH);
+        // }else{
+        //   digitalWrite(LED_YELLOW, LOW);
+        // }
+      } //end while
     }
   }
-  // else if (receiveSample == false && timerStart == true){
-  //   //just do the SPI work and wait to end timer
-  //   timerStart = false;
+  else if (receiveSample == false && timerStart == true){
+    //just do the SPI work and wait to end timer
+    if (nextRead == nextWrite){
+      timer.end();
+      digitalWrite(LED, LOW); //indicate stop timer
+    }
+    timerStart = false;
+    receiveSample = false;
 
-    
-  //   timer.end();
-  //   digitalWrite(LED, LOW); //indicate stop timer
-  // }
+ 
+  }
   
 }
 
@@ -202,12 +259,13 @@ void timerCallback() {
   if (receiveSample == false && nextRead == nextWrite){
     // timerStart = false;
     toggle = !toggle;
+    digitalWrite(LED_YELLOW, HIGH);
   }
   else{
     uint16_t value = *(uint16_t*)&byte_buffer[nextRead];
-    // uint16_t value = toggle ? 0xFFFF : 0x0000;
+
     nextRead = (nextRead+2) % MAX_BUFFER_SIZE;
-    buffer_size--;
+    buffer_size = buffer_size -2;
     
     // Transmit SPI data
     digitalWrite(chipSelectPin, LOW);
